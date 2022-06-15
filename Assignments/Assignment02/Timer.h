33edae61377425afb794586b8b63578fca4dc51c
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2022-06-16 03:44:47 +0900

    Revert "Revert "LabProject 기반으로 교체""
    This reverts commit 7d58b0075849dede6da7b75ae21a383ffa9436c7.

--- Assignments/Assignment02/Timer.h
+++ Assignments/Assignment02/Timer.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <Windows.h>
+//-----------------------------------------------------------------------------
+// File: CGameTimer.h
+//-----------------------------------------------------------------------------
 
-const ULONG MAX_SAMPLE_COUNT = 50;
+const ULONG MAX_SAMPLE_COUNT = 50; // Maximum frame time sample count
 
 class CGameTimer
 {
@@ -9,29 +11,33 @@ public:
 	CGameTimer();
 	virtual ~CGameTimer();
 
-	void Start(){}
-	void Stop(){}
+	void Tick(float fLockFPS = 0.0f);
+	void Start();
+	void Stop();
 	void Reset();
-	void Tick(float fLockFPS = 0.0f);		// 타이머의 시간을 갱신한다
-	unsigned long GetFrameRate(LPTSTR lpszString = NULL, int nCharacters = 0);		//프레임 레이트를 반환한다.
-	float GetTimeElapsed();					// 프레임의 평균 경과 시간을 반환한다.
+
+    unsigned long GetFrameRate(LPTSTR lpszString = NULL, int nCharacters=0);
+    float GetTimeElapsed();
+	float GetTotalTime();
 
 private:
-	bool m_bHardwareHasPerformanceCounter;		// 컴퓨터가 Performance Counter를 가지고 있는가
-	float m_fTimeScale;							// Scale Counter의 양
-	float m_fTimeElapsed;						// 마지막 프레임 이후 지나간 시간
-	__int64 m_nCurrentTime;						// 현재의 시간
-	__int64 m_nLastTime;						// 마지막 프레임의 시간
-	__int64 m_nPerformanceFrequency;			// 컴퓨터의 Performance Frequency
+	double							m_fTimeScale;						
+	float							m_fTimeElapsed;		
 
-	float m_fFrameTime[MAX_SAMPLE_COUNT];		// 프레임 시간을 누적하기 위한 배열
-	ULONG m_nSampleCount;						// 누적된 프레임 횟수
+	__int64							m_nBasePerformanceCounter;
+	__int64							m_nPausedPerformanceCounter;
+	__int64							m_nStopPerformanceCounter;
+	__int64							m_nCurrentPerformanceCounter;
+    __int64							m_nLastPerformanceCounter;
 
-	unsigned long m_nCurrentFrameRate;			// 현재의 프레임 레이트
-	unsigned long m_nFramesPerSecond;			// 초당 프레임 수
-	float m_fFPSTimeElapsed;					// 프레임 레이트 계산 소요 시간
+	__int64							m_nPerformanceFrequencyPerSec;				
 
-	bool m_bStopped;
+    float							m_fFrameTime[MAX_SAMPLE_COUNT];
+    ULONG							m_nSampleCount;
 
+    unsigned long					m_nCurrentFrameRate;				
+	unsigned long					m_nFramesPerSecond;					
+	float							m_fFPSTimeElapsed;		
+
+	bool							m_bStopped;
 };
-
