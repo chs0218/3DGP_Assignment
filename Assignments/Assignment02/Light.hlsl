e2967a5a9b5e344f4553007d603ed68bab3fd837
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2022-06-16 03:38:09 +0900

    LabProject 기반으로 교체

--- Assignments/Assignment02/Light.hlsl
+++ Assignments/Assignment02/Light.hlsl
@@ -1,45 +1,49 @@
-#define MAX_LIGHTS 8 
-#define MAX_MATERIALS 8 
-#define POINT_LIGHT 1
-#define SPOT_LIGHT 2
-#define DIRECTIONAL_LIGHT 3
+//--------------------------------------------------------------------------------------
+#define MAX_LIGHTS			8 
+#define MAX_MATERIALS		16 
+
+#define POINT_LIGHT			1
+#define SPOT_LIGHT			2
+#define DIRECTIONAL_LIGHT	3
+
 #define _WITH_LOCAL_VIEWER_HIGHLIGHTING
 #define _WITH_THETA_PHI_CONES
 //#define _WITH_REFLECT
 
-
 struct MATERIAL
 {
-	float4 m_cAmbient;
-	float4 m_cDiffuse;
-	float4 m_cSpecular; //a = power
-	float4 m_cEmissive;
-};
-struct LIGHT
-{
-	float4 m_cAmbient;
-	float4 m_cDiffuse;
-	float4 m_cSpecular;
-	float3 m_vPosition;
-	float m_fFalloff;
-	float3 m_vDirection;
-	float m_fTheta; //cos(m_fTheta)
-	float3 m_vAttenuation;
-	float m_fPhi; //cos(m_fPhi)
-	bool m_bEnable;
-	int m_nType;
-	float m_fRange;
-	float padding;
+	float4				m_cAmbient;
+	float4				m_cDiffuse;
+	float4				m_cSpecular; //a = power
+	float4				m_cEmissive;
 };
 
-cbuffer cbMaterial : register(b3)
+struct LIGHT
 {
-	MATERIAL gMaterials[MAX_MATERIALS];
+	float4				m_cAmbient;
+	float4				m_cDiffuse;
+	float4				m_cSpecular;
+	float3				m_vPosition;
+	float 				m_fFalloff;
+	float3				m_vDirection;
+	float 				m_fTheta; //cos(m_fTheta)
+	float3				m_vAttenuation;
+	float				m_fPhi; //cos(m_fPhi)
+	bool				m_bEnable;
+	int 				m_nType;
+	float				m_fRange;
+	float				padding;
 };
-cbuffer cbLights : register(b4)
+
+cbuffer cbMaterial : register(b2)
 {
-	LIGHT gLights[MAX_LIGHTS];
-	float4 gcGlobalAmbientLight;
+	MATERIAL			gMaterials[MAX_MATERIALS];
+};
+
+cbuffer cbLights : register(b3)
+{
+	LIGHT				gLights[MAX_LIGHTS];
+	float4				gcGlobalAmbientLight;
 };
 
 float4 DirectionalLight(int nIndex, float3 vNormal, float3 vToCamera)
@@ -53,22 +57,19 @@ float4 DirectionalLight(int nIndex, float3 vNormal, float3 vToCamera)
 		{
 #ifdef _WITH_REFLECT
 			float3 vReflect = reflect(-vToLight, vNormal);
-			fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f),
-				gMaterials[gnMaterial].m_cSpecular.a);
+			fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #else
 #ifdef _WITH_LOCAL_VIEWER_HIGHLIGHTING
 			float3 vHalf = normalize(vToCamera + vToLight);
 #else
 			float3 vHalf = float3(0.0f, 1.0f, 0.0f);
 #endif
-			fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f),
-				gMaterials[gnMaterial].m_cSpecular.a);
+			fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #endif
 		}
 	}
-	return((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) +
-		(gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) +
-		(gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular));
+
+	return((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) + (gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) + (gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular));
 }
 
 float4 PointLight(int nIndex, float3 vPosition, float3 vNormal, float3 vToCamera)
@@ -86,25 +87,20 @@ float4 PointLight(int nIndex, float3 vPosition, float3 vNormal, float3 vToCamera
 			{
 #ifdef _WITH_REFLECT
 				float3 vReflect = reflect(-vToLight, vNormal);
-				fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f),
-					gMaterials[gnMaterial].m_cSpecular.a);
+				fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #else
 #ifdef _WITH_LOCAL_VIEWER_HIGHLIGHTING
 				float3 vHalf = normalize(vToCamera + vToLight);
 #else
 				float3 vHalf = float3(0.0f, 1.0f, 0.0f);
 #endif
-				fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f),
-					gMaterials[gnMaterial].m_cSpecular.a);
+				fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #endif
 			}
 		}
-		float fAttenuationFactor = 1.0f / dot(gLights[nIndex].m_vAttenuation, float3(1.0f,
-			fDistance, fDistance * fDistance));
-		return(((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) +
-			(gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) +
-			(gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular)) *
-			fAttenuationFactor);
+		float fAttenuationFactor = 1.0f / dot(gLights[nIndex].m_vAttenuation, float3(1.0f, fDistance, fDistance*fDistance));
+
+		return(((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) + (gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) + (gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular)) * fAttenuationFactor);
 	}
 	return(float4(0.0f, 0.0f, 0.0f, 0.0f));
 }
@@ -124,42 +120,35 @@ float4 SpotLight(int nIndex, float3 vPosition, float3 vNormal, float3 vToCamera)
 			{
 #ifdef _WITH_REFLECT
 				float3 vReflect = reflect(-vToLight, vNormal);
-				fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f),
-					gMaterials[gnMaterial].m_cSpecular.a);
+				fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #else
 #ifdef _WITH_LOCAL_VIEWER_HIGHLIGHTING
 				float3 vHalf = normalize(vToCamera + vToLight);
 #else
 				float3 vHalf = float3(0.0f, 1.0f, 0.0f);
 #endif
-				fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f),
-					gMaterials[gnMaterial].m_cSpecular.a);
+				fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
 #endif
 			}
 		}
 #ifdef _WITH_THETA_PHI_CONES
 		float fAlpha = max(dot(-vToLight, gLights[nIndex].m_vDirection), 0.0f);
-		float fSpotFactor = pow(max(((fAlpha - gLights[nIndex].m_fPhi) /
-			(gLights[nIndex].m_fTheta - gLights[nIndex].m_fPhi)), 0.0f), gLights[nIndex].m_fFalloff);
+		float fSpotFactor = pow(max(((fAlpha - gLights[nIndex].m_fPhi) / (gLights[nIndex].m_fTheta - gLights[nIndex].m_fPhi)), 0.0f), gLights[nIndex].m_fFalloff);
 #else
-		float fSpotFactor = pow(max(dot(-vToLight, gLights[i].m_vDirection), 0.0f),
-			gLights[i].m_fFalloff);
+		float fSpotFactor = pow(max(dot(-vToLight, gLights[i].m_vDirection), 0.0f), gLights[i].m_fFalloff);
 #endif
-		float fAttenuationFactor = 1.0f / dot(gLights[nIndex].m_vAttenuation, float3(1.0f,
-			fDistance, fDistance * fDistance));
-		return(((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) +
-			(gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) +
-			(gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular)) *
-			fAttenuationFactor * fSpotFactor);
+		float fAttenuationFactor = 1.0f / dot(gLights[nIndex].m_vAttenuation, float3(1.0f, fDistance, fDistance*fDistance));
+
+		return(((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) + (gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) + (gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular)) * fAttenuationFactor * fSpotFactor);
 	}
 	return(float4(0.0f, 0.0f, 0.0f, 0.0f));
 }
 
 float4 Lighting(float3 vPosition, float3 vNormal)
 {
-	float3 vCameraPosition = float3(gvCameraPosition.x, gvCameraPosition.y,
-		gvCameraPosition.z);
+	float3 vCameraPosition = float3(gvCameraPosition.x, gvCameraPosition.y, gvCameraPosition.z);
 	float3 vToCamera = normalize(vCameraPosition - vPosition);
+
 	float4 cColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
 	for (int i = 0; i < MAX_LIGHTS; i++)
 	{
@@ -181,5 +170,7 @@ float4 Lighting(float3 vPosition, float3 vNormal)
 	}
 	cColor += (gcGlobalAmbientLight * gMaterials[gnMaterial].m_cAmbient);
 	cColor.a = gMaterials[gnMaterial].m_cDiffuse.a;
+
 	return(cColor);
 }
+
--- Assignments/Assignment02/stdafx.cpp
+++ Assignments/Assignment02/stdafx.cpp
@@ -4,11 +4,14 @@
 
 #include "stdafx.h"
 
-UINT gnCbvSrvDescriptorIncrementSize = 32;
+// TODO: 필요한 추가 헤더는
+// 이 파일이 아닌 STDAFX.H에서 참조합니다.
 
-ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, void* pData, UINT nBytes, D3D12_HEAP_TYPE d3dHeapType, D3D12_RESOURCE_STATES d3dResourceStates, ID3D12Resource** ppd3dUploadBuffer)
+//UINT gnCbvSrvDescriptorIncrementSize;
+
+ID3D12Resource *CreateBufferResource(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList, void *pData, UINT nBytes, D3D12_HEAP_TYPE d3dHeapType, D3D12_RESOURCE_STATES d3dResourceStates, ID3D12Resource **ppd3dUploadBuffer)
 {
-	ID3D12Resource* pd3dBuffer = NULL;
+	ID3D12Resource *pd3dBuffer = NULL;
 
 	D3D12_HEAP_PROPERTIES d3dHeapPropertiesDesc;
 	::ZeroMemory(&d3dHeapPropertiesDesc, sizeof(D3D12_HEAP_PROPERTIES));
@@ -36,7 +39,7 @@ ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 	if (d3dHeapType == D3D12_HEAP_TYPE_UPLOAD) d3dResourceInitialStates = D3D12_RESOURCE_STATE_GENERIC_READ;
 	else if (d3dHeapType == D3D12_HEAP_TYPE_READBACK) d3dResourceInitialStates = D3D12_RESOURCE_STATE_COPY_DEST;
 
-	HRESULT hResult = pd3dDevice->CreateCommittedResource(&d3dHeapPropertiesDesc, D3D12_HEAP_FLAG_NONE, &d3dResourceDesc, d3dResourceInitialStates, NULL, __uuidof(ID3D12Resource), (void**)&pd3dBuffer);
+	HRESULT hResult = pd3dDevice->CreateCommittedResource(&d3dHeapPropertiesDesc, D3D12_HEAP_FLAG_NONE, &d3dResourceDesc, d3dResourceInitialStates, NULL, __uuidof(ID3D12Resource), (void **)&pd3dBuffer);
 
 	if (pData)
 	{
@@ -47,11 +50,11 @@ ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 			if (ppd3dUploadBuffer)
 			{
 				d3dHeapPropertiesDesc.Type = D3D12_HEAP_TYPE_UPLOAD;
-				pd3dDevice->CreateCommittedResource(&d3dHeapPropertiesDesc, D3D12_HEAP_FLAG_NONE, &d3dResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, NULL, __uuidof(ID3D12Resource), (void**)ppd3dUploadBuffer);
+				pd3dDevice->CreateCommittedResource(&d3dHeapPropertiesDesc, D3D12_HEAP_FLAG_NONE, &d3dResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, NULL, __uuidof(ID3D12Resource), (void **)ppd3dUploadBuffer);
 
 				D3D12_RANGE d3dReadRange = { 0, 0 };
-				UINT8* pBufferDataBegin = NULL;
-				(*ppd3dUploadBuffer)->Map(0, &d3dReadRange, (void**)&pBufferDataBegin);
+				UINT8 *pBufferDataBegin = NULL;
+				(*ppd3dUploadBuffer)->Map(0, &d3dReadRange, (void **)&pBufferDataBegin);
 				memcpy(pBufferDataBegin, pData, nBytes);
 				(*ppd3dUploadBuffer)->Unmap(0, NULL);
 
@@ -72,8 +75,8 @@ ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 		case D3D12_HEAP_TYPE_UPLOAD:
 		{
 			D3D12_RANGE d3dReadRange = { 0, 0 };
-			UINT8* pBufferDataBegin = NULL;
-			pd3dBuffer->Map(0, &d3dReadRange, (void**)&pBufferDataBegin);
+			UINT8 *pBufferDataBegin = NULL;
+			pd3dBuffer->Map(0, &d3dReadRange, (void **)&pBufferDataBegin);
 			memcpy(pBufferDataBegin, pData, nBytes);
 			pd3dBuffer->Unmap(0, NULL);
 			break;
@@ -85,3 +88,81 @@ ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 	return(pd3dBuffer);
 }
 
+CGameObject **LoadGameObjectsFromFile(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList, char *pstrFileName, int *pnGameObjects)
+{
+	FILE *pFile = NULL;
+	::fopen_s(&pFile, pstrFileName, "rb");
+	::rewind(pFile);
+
+	char pstrToken[64] = { '\0' };
+	char pstrGameObjectName[64] = { '\0' };
+	char pstrFilePath[64] = { '\0' };
+
+	BYTE nStrLength = 0, nObjectNameLength = 0;
+	UINT nReads = 0, nMaterials = 0;
+	size_t nConverted = 0;
+
+	nReads = (UINT)::fread(&nStrLength, sizeof(BYTE), 1, pFile);
+	nReads = (UINT)::fread(pstrToken, sizeof(char), nStrLength, pFile); //"<GameObjects>:"
+	nReads = (UINT)::fread(pnGameObjects, sizeof(int), 1, pFile);
+
+	CGameObject **ppGameObjects = new CGameObject*[*pnGameObjects];
+
+	CGameObject *pGameObject = NULL, *pObjectFound = NULL;
+	for (int i = 0; i < *pnGameObjects; i++)
+	{
+		nReads = (UINT)::fread(&nStrLength, sizeof(BYTE), 1, pFile);
+		nReads = (UINT)::fread(pstrToken, sizeof(char), nStrLength, pFile); //"<GameObject>:"
+		nReads = (UINT)::fread(&nObjectNameLength, sizeof(BYTE), 1, pFile);
+		nReads = (UINT)::fread(pstrGameObjectName, sizeof(char), nObjectNameLength, pFile);
+		pstrGameObjectName[nObjectNameLength] = '\0';
+
+		nReads = (UINT)::fread(&nStrLength, sizeof(BYTE), 1, pFile);
+		nReads = (UINT)::fread(pstrToken, sizeof(char), nStrLength, pFile); //"<Materials>:"
+		nReads = (UINT)::fread(&nMaterials, sizeof(int), 1, pFile);
+
+		pGameObject = new CGameObject(nMaterials);
+		strcpy_s(pGameObject->m_pstrName, 64, pstrGameObjectName);
+
+		CGameObject *pObjectFound = NULL;
+		for (int j = 0; j < i; j++)
+		{
+			if (!strcmp(pstrGameObjectName, ppGameObjects[j]->m_pstrName))
+			{
+				pObjectFound = ppGameObjects[j];
+				pGameObject->SetMesh(ppGameObjects[j]->m_pMesh);
+				for (UINT k = 0; k < nMaterials; k++) pGameObject->SetMaterial(k, ppGameObjects[j]->m_ppMaterials[k]);
+				break;
+			}
+		}
+
+		XMFLOAT4 xmf4AlbedoColor = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), xmf4EmissionColor = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		for (UINT k = 0; k < nMaterials; k++)
+		{
+			if (!pObjectFound) pGameObject->SetMaterial(k, rand() % MAX_SCENE_MATERIALS);
+
+			nReads = (UINT)::fread(&xmf4AlbedoColor, sizeof(float), 4, pFile);
+			if (!pObjectFound) pGameObject->SetAlbedoColor(k, xmf4AlbedoColor);
+
+			nReads = (UINT)::fread(&xmf4EmissionColor, sizeof(float), 4, pFile);
+			if (!pObjectFound) pGameObject->SetEmissionColor(k, xmf4EmissionColor);
+		}
+
+		nReads = (UINT)::fread(&pGameObject->m_xmf4x4World, sizeof(float), 16, pFile);
+
+		if (!pObjectFound)
+		{
+			strcpy_s(pstrFilePath, 64, "Models/");
+			strcpy_s(pstrFilePath + 7, 64 - 7, pstrGameObjectName);
+			strcpy_s(pstrFilePath + 7 + nObjectNameLength, 64 - 7 - nObjectNameLength, ".bin");
+			CMesh *pMesh = new CMesh(pd3dDevice, pd3dCommandList, pstrFilePath);
+			pGameObject->SetMesh(pMesh);
+		}
+
+		ppGameObjects[i] = pGameObject;
+	}
+
+	::fclose(pFile);
+
+	return(ppGameObjects);
+}
